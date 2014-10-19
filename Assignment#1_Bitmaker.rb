11dcff0032f53515f5b08ec4c79295fdc0f705ec
Dayna Lowe <dayna.c.lowe@gmail.com>
2014-10-19 18:12:17 -0400

    Bitmaker Labs Prepwork

---
+++ Assignment#1_Bitmaker.rb
@@ -0,0 +1,12 @@
+for bit in 1..100
+   if bit % 3 == 0
+      puts "Bitmaker"
+   elsif bit % 5 == 0
+      puts "Labs"
+  else bit % 5 == 0 and bit % 3 == 0
+      puts "Cats"
+   unless false 
+      puts bit
+   end
+end
+end
---
+++ Assignment#1_FavouriteFoods.rb
@@ -0,0 +1,3 @@
+["mac'n'cheeze", "kale cakes", "palak paneer", "arugula", "pizza"].each do |noms|
+puts "I love #{noms}."
+end
